150382c2b098607277b86b686a68ea083a4c99f8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-12-07 03:14:20 +0900

    ll1

---
+++ src/ll/ll1.cc
@@ -0,0 +1,28 @@
+#include "ll1.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+// template <class T> class Node {
+//   public:
+//     T val;
+//     Node *next;
+//     Node(T val) : val(val) {}
+// };
+
+// template<class T> Node<T>::Node(T val) : val(val){};
+
+// template <class T> void solve(Node<T> *head) {
+//     unordered_set<T> exist;
+//     Node<T> *cursor = head, *prev;
+//     while(cursor != nullptr) {
+//         if(exist.find(cursor->val) == exist.end()) {
+//             exist.insert(cursor->val);
+//             prev = cursor;
+//             cursor = cursor->next;
+//             continue;
+//         }
+//         prev->next = cursor->next;
+//         delete cursor;
+//         cursor = prev->next;
+//     }
+// }
---
+++ tests/ll/ll1.cc
@@ -0,0 +1,31 @@
+#include "ll/ll1.h"
+#include <gtest/gtest.h>
+
+TEST(ll1, 1) {
+    Node<int> *a = new Node<int>(0);
+    Node<int> *a1 = new Node<int>(1);
+    Node<int> *a2 = new Node<int>(2);
+    Node<int> *a3 = new Node<int>(3);
+    a->next = a1;
+    a1->next = a2;
+    a2->next = a3;
+    solve(a);
+    EXPECT_EQ(0, a->val);
+    EXPECT_EQ(1, a->next->val);
+    EXPECT_EQ(2, a->next->next->val);
+    EXPECT_EQ(3, a->next->next->next->val);
+}
+
+TEST(ll1, 2) {
+    Node<int> *a = new Node<int>(0);
+    Node<int> *a1 = new Node<int>(0);
+    Node<int> *a2 = new Node<int>(0);
+    Node<int> *a3 = new Node<int>(3);
+    a->next = a1;
+    a1->next = a2;
+    a2->next = a3;
+    solve(a);
+    EXPECT_EQ(0, a->val);
+    EXPECT_EQ(3, a->next->val);
+    EXPECT_EQ(nullptr, a->next->next);
+}
