1427ebba04298ebe1324edb3dc1d3fbf6183aca8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-02 12:30:32 +0900

    oop10

---
+++ src/oop/oop10.cc
@@ -0,0 +1,48 @@
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Cell final {
+//   public:
+// 	Cell(bool is_bomb, int num_of_neighbor_bombs);
+// private:
+//   bool is_bomb;
+//   int num_of_neigthbor_bombs;
+//   bool visible;
+// };
+
+// class Grid final {
+//  public:
+//   Grid(vector<vector<Cell>> grid);
+//   bool open(int row, int column) {
+//     return false;
+//   }
+//  private:
+//   vector<vector<Cell>> grid;
+//   int num_of_open_cell;
+// };
+
+// class GridFactory final {
+
+//   /// random
+//   //
+//   https://www.delftstack.com/ja/howto/cpp/generate-random-number-in-range-cpp/
+//   Grid create(int n, int number_of_bomb) {
+//     mt19937 mt;
+//    	random_device rnd;
+//     mt.seed(rnd());
+//     mt();
+//     vector<vector<int>> int
+//     return Grid
+//   }
+// };
+
+// class Player final {
+// public:
+//   pair<int, int> selectCell(Grid grid) {
+//   }
+// };
+
+// int main() {
+//   return 0;
+// }
