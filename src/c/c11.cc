6eeb4f9c227129e8158b51e30a9fa2372c26c2a8
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-03-09 13:32:07 +0900

    c11

--- src/c/c11.cc
+++ src/c/c11.cc
@@ -4,14 +4,17 @@ using namespace std;
 int **solve(int row, int column) {
   int **res = (int **)malloc(sizeof(int) * row * column + sizeof(int *) * row);
   for (int i = 0; i < row; i++) {
-    res[i] = (int *)res + sizeof(int *) * row + sizeof(int) * i;
+    cout << sizeof(int *) * row + sizeof(int) * i * column << endl;
+    res[i] = (int *)res + sizeof(int *) * row + sizeof(int) * i * column;
   }
+  cout << res[0] - (int *)res << endl;
   return res;
 }
 
 // int main() {
 //   int** a = solve(2, 3);
 //   int c = 0;
+//   /*
 //   for(int i = 0;i<2;i++) {
 //     for(int j = 0;j<3;j++) {
 //       a[i][j] = c++;
@@ -22,5 +25,6 @@ int **solve(int row, int column) {
 //       cout << a[i][j] << endl;
 //     }
 //   }
+//   */
 //   return 0;
 // }
